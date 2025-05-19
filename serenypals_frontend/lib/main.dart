7ebb12f0a46b706d031f28cbd655f247aa304dfb
ThanosTHINOS <164857755+ThanosTHINOS@users.noreply.github.com>
2025-05-19 21:30:16 +0800

    topup(done)
    tino

--- serenypals_frontend/lib/main.dart
+++ serenypals_frontend/lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:serenypals_frontend/screen/loginscreen.dart';
+import 'package:serenypals_frontend/screen/registerscreen.dart';
 import 'package:serenypals_frontend/screen/topup.dart';
 
-void main() => runApp(const Mytino());
+void main() {
+  runApp(const Mytino());
+}
 
 class Mytino extends StatelessWidget {
   const Mytino({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'SerenyPals',
       theme: ThemeData(fontFamily: 'Poppins'),
-      home: const DiamondTopUpPage(),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/topup': (context) => const DiamondTopUpPage(),
+      },
     );
   }
 }
-
