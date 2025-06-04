57a5639c6ce980e2374c73d44870fec4311f1441
Tiffany <148511931+Roczantya@users.noreply.github.com>
2025-06-04 14:57:06 +0800

    pwngantian nama

--- serenypals_frontend/lib/views/view/splashscreen.dart
+++ serenypals_frontend/lib/views/view/splashscreen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../utils/color.dart';
+import '../../utils/color.dart';
 
 class SplashScreen extends StatefulWidget {
   final Duration delay;
